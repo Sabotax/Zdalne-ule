4e85a45d76d34ab5d4869443df84053c299a3b52
Daniel <daniel.r5@tlen.pl>
2022-04-26 18:51:38 +0200

    v3

---
+++ programy/offline_zbieranie_danych_v3/offline_zbieranie_danych_v3/offline_zbieranie_danych_v3.ino
@@ -0,0 +1,127 @@
+/*
+ * Spis pinów
+ * 
+ * GPIO11 - OneWire Dallas temperatura
+ * 
+ * GPIO7 - HX711 data
+ * GPIO6 - HX711 clk
+ * 
+ * Karta pamięci
+ * MISO D6
+ * MOSI D7
+ * SCK D5
+ * CS D8
+ * 
+ */
+
+// TEMPERATURA
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 11
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+//--zmienne
+float tempC;
+
+// WAGA
+#define pin_waga_data 7
+#define pin_waga_clk 6
+#include "HX711.h"
+HX711 scale(pin_waga_data,pin_waga_clk);
+float calibration_factor = 1;
+//-- zmienne
+float pomiar_waga;
+
+// KARTA SD
+#include <SPI.h>
+#include <SD.h>
+#define pin_CS D8
+File myFile;
+//--zmienne
+String nazwa = "dataK.txt";
+String text = "";
+
+// INNE ZMIENNE
+boolean DEBUG = true;
+boolean setup_error = false;
+
+// POZOSTALE FUNKCJE
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Start programu");
+
+  // TEMPERATURA
+  sensors.begin();
+  if(DEBUG) Serial.println("Temperatura zainicjalizowana pomyslnie");
+
+  // WAGA
+  scale.set_scale(calibration_factor);
+  scale.tare();
+  long zero_factor = scale.read_average();
+
+  // KARTA SD
+  if (DEBUG) Serial.print("Initializing SD card...");
+  if( !SD.begin(pin_CS) ) {
+    if (DEBUG) Serial.println("initialization failed!");
+    setup_error = true;
+  }
+  else {
+    if (DEBUG) Serial.println("initialization done.");
+  }
+  delay(1000);
+
+  pinMode(LED_BUILTIN,OUTPUT);
+  while(setup_error) {
+    digitalWrite(LED_BUILTIN,HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
+
+  // --------POMIARY
+  // TEMPERAUTRA - POMIAR
+  sensors.requestTemperatures();
+  tempC = sensors.getTempCByIndex(0);
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    if (DEBUG) Serial.print("Temperature for the device 1 (index 0) is: ");
+    if (DEBUG) Serial.println(tempC);
+  } 
+  else
+  {
+    if (DEBUG) Serial.println("Error: Could not read temperature data");
+    loop_error = true;
+  }
+
+  // WAGA POMIAR
+  pomiar_waga = scale.get_units();
+  if (DEBUG) Serial.println("Odczyt wagi: " + String(pomiar_waga));
+
+  // KARTA SD ZAPIS
+  myFile = SD.open(nazwa, FILE_WRITE);
+
+  if (myFile) {
+    if (DEBUG) Serial.print("Writing to " + nazwa);
+    //text = toString(now) + "," + String(tempC) + "," + String(pomiar_waga);
+    myFile.print(String(tempC) + ",");
+    myFile.flush();
+    myFile.println(String(pomiar_waga));
+    myFile.flush();
+    myFile.close();
+    if (DEBUG) Serial.println("done.");
+  } else {
+    if (DEBUG) Serial.println("error opening " + nazwa);
+    loop_error = true;
+  }
+
+  if (DEBUG) Serial.println();
+  if (DEBUG) Serial.flush();
+
+  // SLEEP
+  //ESP.deepSleep(60e6);
+  delay(10e3);
+}
+
+void loop() {
+}
