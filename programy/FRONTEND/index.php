d7146b19ea3e5f187bc8fb318e951d0b0bdcd905
Sabotax <62028372+Sabotax@users.noreply.github.com>
2022-06-26 17:23:05 +0200

    front start

---
+++ programy/FRONTEND/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div id="container">
+            <div id="top">
+                <div id="topbar">
+                    bars
+                </div>
+            </div>
+            <div id="center">
+                <div>
+                    <div id="pasieka_select">
+
+                    </div>
+                    <div id="pasieka_opcje">
+
+                    </div>
+                </div>
+                <div id="ul">
+                    <div id="ul_select">
+                    </div>
+                    <div id="ul_opcje">
+
+                    </div>
+                </div>
+                <div id="ul_showcase">
+                    
+                </div>
+            </div>
+        </div>
+        <script src="script.js" async defer></script>
+    </body>
+</html>
