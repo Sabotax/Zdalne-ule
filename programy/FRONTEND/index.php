59d61f2a89fbb5d956e67f0c734fdb56a30bd2e0
Sabotax <62028372+Sabotax@users.noreply.github.com>
2022-06-28 14:44:49 +0200

    front
    merge folderów php i front oraz praca na nich w htdocs

--- programy/FRONTEND/index.php
+++ programy/FRONTEND/index.php
@@ -15,25 +15,40 @@
                     bars
                 </div>
             </div>
-            <div id="center">
-                <div>
+            <div id="left">
+                <div id="pasieka">
                     <div id="pasieka_select">
-
+                        <p>Wybór pasieki</p>
+                        <select id="lista_pasieka">
+                            <?php
+                            // wszytkie esp-master z bazy
+                            ?>
+                        </select>
+                        <button id="btn_pasieka_pokaz" disabled>Pokaż</buton>
                     </div>
                     <div id="pasieka_opcje">
-
+                        <button id="btn_pasieka_zmien_nazwe" disabled>Zmień nazwę</button>
                     </div>
                 </div>
                 <div id="ul">
                     <div id="ul_select">
+                        <p>Wybór ula</p>
+                        <select id="lista_ule" disabled>
+                            <?php
+                            // wszytkie esp-slave dla danego esp-master z bazy
+                            ?>
+                        </select>
+                        <button id="btn_ule_pokaz" disabled>Pokaż</buton>
                     </div>
                     <div id="ul_opcje">
-
+                        <button id="btn_ul_zmien_nazwe" disabled>Zmień nazwę</button>
                     </div>
                 </div>
                 <div id="ul_showcase">
                     
                 </div>
+            </div>
+            <div id="center">
             </div>
         </div>
         <script src="script.js" async defer></script>
