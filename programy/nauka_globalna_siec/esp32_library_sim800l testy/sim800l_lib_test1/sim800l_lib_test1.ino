5fbc4197d955049231959fef5e4fe6679c668b4b
Sabotax <62028372+Sabotax@users.noreply.github.com>
2022-11-02 21:57:36 +0100

    Update sim800l_lib_test1.ino

--- programy/nauka_globalna_siec/esp32_library_sim800l testy/sim800l_lib_test1/sim800l_lib_test1.ino
+++ programy/nauka_globalna_siec/esp32_library_sim800l testy/sim800l_lib_test1/sim800l_lib_test1.ino
@@ -10,12 +10,46 @@ unsigned int TX_PIN = 17;
 
 HTTP http(2, 9600); //, RX_PIN, TX_PIN, RST_PIN
 
+char buffer_gps[80];
+char buffer_battery[64];
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("start");
+  Serial.println(F("start"));
   delay(5000);
+  
+  Serial.println(F("readVoltage:"));
   Serial.println(String(http.readVoltage()));
+  
+  Serial.println(F("read GPS"));
+  http.readGpsLocation(buffer_gps);
+  Serial.println(buffer_gps);
+
+  Serial.println(F("readVoltagePercentage"));
+  http.readVoltagePercentage(buffer_battery);
+  Serial.println(buffer_battery);
+
+  Serial.println(F("Usypiam SIM (tryb snu 2) i czekam 10 sekund"));
+  http.sleep(false);
+  delay(10000);
+  Serial.println(F("Budze SIM"));
+  http.wakeUp();
+  delay(3000);
+
+  Serial.println(F("Wysyłam example json post"));
+  http.configureBearer("your.mobile.service.provider.apn");
+  http.connect();
+
+  char response[256];
+  // wlaczanie ssl poprzez wrzucenie do linka https:// (samo wykrywa i ustawia biblioteka)
+  Result result = http.post("daniel.rozycki.student.put.poznan.pl", "{\"date\":\"12345678\"}", response);
+  Serial.println(F("Odpowiedz"));
+  Serial.println(response);
+
+  http.disconnect();
+  
+  
 }
 
 void loop() {
